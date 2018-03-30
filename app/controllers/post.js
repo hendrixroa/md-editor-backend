a96f2ff93251725c195da024ca4831a3aec59d2f
Hendrix Roa <hendrix.alex87@gmail.com>
2018-03-29 23:28:52 -0500

    crud terminado

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -3,24 +3,31 @@ module.exports.Post = () => {
   const db = require('../database/querys').Querys()
 
   const listPosts = async (req, res) => {
-    const { rows } = await db.query('SELECT * FROM posts;', [])
-    res.send(rows[0])
-  }
-
-  const createPost = async (req, res) => {
-    const rows = await db.query('INSERT INTO md_editor.posts (post) values (?);', [req.body.post])
+    const rows = await db.query('SELECT * FROM md_editor.posts;', [])
     res.json(rows)
   }
 
-  const editPost = () => {
-    
+  const createPost = async (req, res) => {
+    if(!req.body.post)
+      return res.status(403).send({ status: 403, message: 'The post is required'})
+    const rows = await db.query('INSERT INTO md_editor.posts (post) VALUES (?);', [req.body.post])
+    res.json(rows)
   }
 
-  const deletePost = () => {
-    
+  const editPost = async (req, res) => {
+    if(!req.body.post)
+      return res.status(403).send({ status: 403, message: 'The post is required'})
+    const rows = await db.query('UPDATE md_editor.posts SET post = ? WHERE id = ?;', [req.body.post, req.params.id])
+    res.json(rows)
+  }
+
+  const deletePost = async (req, res) => {
+    const rows = await db.query('DELETE FROM md_editor.posts WHERE id = ?;',  [req.params.id])
+    res.json(rows)
   }
 
   return {
+    listPosts: listPosts,
     createPost: createPost,
     editPost: editPost,
     deletePost: deletePost
--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(parser.text())
 app.use(parser.json({ type: 'application/json'}))
 
 app.route('/post')
+  .get(post.listPosts)
   .post(post.createPost)
+
+app.route('/post/:id')
+  .put(post.editPost)
+  .delete(post.deletePost)
  
 const port = process.env.PORT || config.server.port
 const server = app.listen(port, () => {
