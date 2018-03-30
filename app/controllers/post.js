c05243bb56368e77aefdc6b734d94476f4f6be41
Hendrix Roa <hendrix.alex87@gmail.com>
2018-03-30 13:59:31 -0500

    fix config vars env

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -3,26 +3,26 @@ module.exports.Post = () => {
   const db = require('../database/querys').Querys()
 
   const listPosts = async (req, res) => {
-    const rows = await db.query('SELECT * FROM documents;', []).catch(err => { res.status(403).json({ body: err }) })
+    const rows = await db.query(`SELECT * FROM ${process.env.DATABASE}.documents;`, []).catch(err => { res.status(403).json({ body: err }) })
     res.json(rows)
   }
 
   const createPost = async (req, res) => {
     if(!req.body.post)
       return res.status(403).send({ status: 403, message: 'The post is required'}).catch(err => { res.status(403).json({ body: err }) })
-    const rows = await db.query('INSERT INTO documents (post) VALUES (?);', [req.body.post])
+    const rows = await db.query(`INSERT INTO ${process.env.DATABASE}.documents (post) VALUES (?);`, [req.body.post])
     res.json(rows)
   }
 
   const editPost = async (req, res) => {
     if(!req.body.post)
       return res.status(403).send({ status: 403, message: 'The post is required'}).catch(err => { res.status(403).json({ body: err }) })
-    const rows = await db.query('UPDATE documents SET post = ? WHERE id = ?;', [req.body.post, req.params.id])
+    const rows = await db.query(`UPDATE ${process.env.DATABASE}.documents SET post = ? WHERE id = ?;`, [req.body.post, req.params.id])
     res.json(rows)
   }
 
   const deletePost = async (req, res) => {
-    const rows = await db.query('DELETE FROM documents WHERE id = ?;', [req.params.id]).catch(err => { res.status(403).json({ body: err }) })
+    const rows = await db.query(`DELETE FROM ${process.env.DATABASE}.documents WHERE id = ?;`, [req.params.id]).catch(err => { res.status(403).json({ body: err }) })
     res.json(rows)
   }
 
